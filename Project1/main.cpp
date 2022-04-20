31659159f4bd5c17f075c9295f4df322494a9e04
YuukiSakihiro <k021g1259@g.neec.ac.jp>
2022-04-20 13:26:44 +0900

    色付け

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -6,6 +6,8 @@
 #pragma comment(lib, "dxgi.lib")
 #include <vector>
 #include <string>
+#include <DirectXMath.h>
+using namespace DirectX;
 // Windowsアプリでのエントリーポイント(main関数)
 // ウィンドウプロシージャ
 LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
@@ -180,7 +182,58 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		result = device->CreateFence(fenceVal, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence));
 	
 	// DirectX初期化処理 ここまで
-// ゲームループ
+		//描画初期化処理
+		// 頂点データ
+		XMFLOAT3 vertices[] = {
+		{ -0.5f, -0.5f, 0.0f }, // 左下
+		{ -0.5f, +0.5f, 0.0f }, // 左上
+		{ +0.5f, -0.5f, 0.0f }, // 右下
+		};
+		// 頂点データ全体のサイズ = 頂点データ一つ分のサイズ * 頂点データの要素数
+		UINT sizeVB = static_cast<UINT>(sizeof(XMFLOAT3) * _countof(vertices));
+		// 頂点バッファの設定
+		D3D12_HEAP_PROPERTIES heapProp{}; // ヒープ設定
+		heapProp.Type = D3D12_HEAP_TYPE_UPLOAD; // GPUへの転送用
+		// リソース設定
+		D3D12_RESOURCE_DESC resDesc{};
+		resDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+		resDesc.Width = sizeVB; // 頂点データ全体のサイズ
+		resDesc.Height = 1;
+		resDesc.DepthOrArraySize = 1;
+		resDesc.MipLevels = 1;
+		resDesc.SampleDesc.Count = 1;
+		resDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+		// 頂点バッファの生成
+		ID3D12Resource* vertBuff = nullptr;
+		result = device->CreateCommittedResource(
+			&heapProp, // ヒープ設定
+			D3D12_HEAP_FLAG_NONE,
+			&resDesc, // リソース設定
+			D3D12_RESOURCE_STATE_GENERIC_READ,
+			nullptr,
+			IID_PPV_ARGS(&vertBuff));
+		assert(SUCCEEDED(result));
+		// GPU上のバッファに対応した仮想メモリ(メインメモリ上)を取得
+		XMFLOAT3* vertMap = nullptr;
+		result = vertBuff->Map(0, nullptr, (void**)&vertMap);
+		assert(SUCCEEDED(result));
+		// 全頂点に対して
+		for (int i = 0; i < _countof(vertices); i++) {
+			vertMap[i] = vertices[i]; // 座標をコピー
+		}
+		// 繋がりを解除
+		vertBuff->Unmap(0, nullptr);
+		// 頂点バッファビューの作成
+		D3D12_VERTEX_BUFFER_VIEW vbView{};
+		// GPU仮想アドレス
+		vbView.BufferLocation = vertBuff->GetGPUVirtualAddress();
+		// 頂点バッファのサイズ
+		vbView.SizeInBytes = sizeVB;
+		// 頂点1つ分のデータサイズ
+		vbView.StrideInBytes = sizeof(XMFLOAT3);
+		
+		// ゲームループ
 	while (true) {
 		// メッセージがある?
 		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
